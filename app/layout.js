6b95248aeda3c9294b6b89d6b03df29b6612bdbd
ggg12323u-arch <ggg12323u@gmail.com>
2026-08-21 16:18:50 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,29 @@
 export const metadata = {
   title: 'DroJent Web',
   description: 'Приватный мессенджер',
+  manifest: '/manifest.json', // Это свяжет твой файл манифеста
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+      <head>
+        {/* Скрипт для регистрации Service Worker */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if ('serviceWorker' in navigator) {
+                window.addEventListener('load', function() {
+                  navigator.serviceWorker.register('/sw.js');
+                });
+              }
+            `,
+          }}
+        />
+      </head>
+      <body style={{ margin: 0, padding: 0 }}>
+        {children}
+      </body>
     </html>
   );
 }
