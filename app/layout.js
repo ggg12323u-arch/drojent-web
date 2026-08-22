50acc390d87366f2ff7d98ac879e2dba2191c81b
ggg12323u-arch <ggg12323u@gmail.com>
2026-08-22 09:44:54 +0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,12 +2,22 @@ export const metadata = {
   title: 'DroJent Web',
   description: 'Приватный мессенджер',
   manifest: '/manifest.json', // Это свяжет твой файл манифеста
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'black-translucent',
+    title: 'DroJent',
+  },
+  icons: {
+    icon: '/icon.jpg',
+    apple: '/icon.jpg',
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
       <head>
+        <meta name="theme-color" content="#0b0f19" />
         {/* Скрипт для регистрации Service Worker */}
         <script
           dangerouslySetInnerHTML={{
